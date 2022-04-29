823b9fc3d60c40418dc9a14f8a0f1c92f1bb4418
Philippe <philippe.vial90@gmail.com>
2022-04-29 13:27:23 +0200

    Add PizzaController.java

---
+++ src/main/java/org/example/controller/PizzaController.java
@@ -0,0 +1,39 @@
+package org.example.controller;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import org.example.pojo.Pizza;
+import org.example.repository.PizzaRepository;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import static io.micronaut.http.HttpStatus.CONFLICT;
+import static io.micronaut.http.HttpStatus.CREATED;
+
+@Controller("/pizzas")
+public class PizzaController {
+
+    private final PizzaRepository pizzaService;
+
+
+    public PizzaController(PizzaRepository pizzaService) {
+        this.pizzaService = pizzaService;
+    }
+
+    @Get
+    Publisher<Pizza> list() {
+        return pizzaService.list();
+    }
+
+    @Post
+    Mono<HttpStatus> save(@NonNull @NotNull @Valid Pizza fruit) {
+        return pizzaService.save(fruit)
+                .map(added -> added ? CREATED : CONFLICT);
+    }
+}
