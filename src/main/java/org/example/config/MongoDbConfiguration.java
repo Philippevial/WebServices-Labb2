cc42f385bcb469e7a8d535c825487d2a9be82593
Philippe <philippe.vial90@gmail.com>
2022-04-29 13:27:32 +0200

    Add MongoDbConfiguration.java

---
+++ src/main/java/org/example/config/MongoDbConfiguration.java
@@ -0,0 +1,13 @@
+package org.example.config;
+
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.naming.Named;
+
+@ConfigurationProperties("db")
+public interface MongoDbConfiguration extends Named {
+
+    @NonNull
+    String getCollection();
+}
